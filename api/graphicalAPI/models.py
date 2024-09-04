2530ad7ac17532b719ab3a9838e3c0a3efcfe259
Brian Olsen <btolsen131@gmail.com>
2024-09-03 20:55:40 -0400

    Change api backend to python django

---
+++ api/graphicalAPI/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class NoteNode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.TextField(null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    children = models.ManyToManyField('self', symmetrical=False, blank=True)
+
+
--- api/models/Node.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Node struct{
-	ID       int    `json:"id"`
-	Title    string `json:"title"`
-	Body     string `json:"body"`
-	Children []int `json:"children"` //Parsed from string
-}
